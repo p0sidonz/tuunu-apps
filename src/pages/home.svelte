2a32dcd497bbfb26fb681eb9a222f00706928b25
Ankit Singh <ethicalankitx0@gmail.com>
2022-06-18 18:49:15 +0530

    fixed things

--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -18,18 +18,74 @@
     Searchbar,
     ListItem,
     List,
-
     Icon,
   } from "framework7-svelte";
-  import { onMount } from 'svelte';
 
-    // Router component will receive f7router prop with current Router instance
-    export let f7router ;
+  import axios from "axios";
+  import { onMount } from "svelte";
+
+  let movies = [
+    // {
+    //   name: "Titanic",
+    //   photo:
+    //     "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg",
+    // },
+    // {
+    //   name: "Titanic",
+    //   photo:
+    //     "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg",
+    // },
+    // {
+    //   name: "Titanic",
+    //   photo:
+    //     "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg",
+    // },
+    // {
+    //   name: "Titanic",
+    //   photo:
+    //     "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg",
+    // },
+  ];
+
+  onMount(async () => {
+    const res = await axios.get("http://127.0.0.1:8000/movies/movies/");
+    movies = res.data.results;
+    next = res.data.next;
+    console.log(res);
+  });
+
+  // // Router component will receive f7router prop with current Router instance
+  export let f7router;
   // Router component will receive f7route prop with current route data
   export let f7route;
+
+  let allowInfinite = true;
+  let showPreloader = true;
+  let next;
+
+  async function loadMore() {
+    if (!allowInfinite) return;
+    allowInfinite = false;
+
+    if (!next) {
+      allowInfinite = false;
+      return;
+    }
+    const res = await axios.get(next);
+    movies = [...movies, ...res.data.results];
+    next = res.data.next;
+
+    allowInfinite = true;
+  }
 </script>
 
-<Page name="home">
+<Page
+  name="home"
+  infinite
+  infiniteDistance={50}
+  infinitePreloader={showPreloader}
+  onInfinite={loadMore}
+>
   <!-- Top Navbar -->
   <Navbar sliding={true}>
     <NavLeft>
@@ -43,12 +99,18 @@
     <NavTitle sliding>Watchmovies</NavTitle>
 
     <NavRight>
-      <Link onClick={() => f7router.navigate('/settings/')} searchbarEnable=".searchbar-demo" iconIos="f7:search" iconAurora="f7:search" iconMd="material:search"></Link>
+      <Link
+        onClick={() => f7router.navigate("/settings/")}
+        searchbarEnable=".searchbar-demo"
+        iconIos="f7:search"
+        iconAurora="f7:search"
+        iconMd="material:search"
+      />
     </NavRight>
   </Navbar>
 
-  <BlockTitle>Trending movies</BlockTitle>
-  <Swiper navigation speed={500} slidesPerView={3} spaceBetween={20}>
+  <!-- <BlockTitle>Trending movies</BlockTitle> -->
+  <!-- <Swiper navigation speed={500} slidesPerView={3} spaceBetween={20}>
     <SwiperSlide>
       <img
         src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"
@@ -58,74 +120,96 @@
         srcset=""
       /></SwiperSlide
     >
-    <SwiperSlide><img
-      src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"
-      alt=""
-      height="180px"
-      width="100px"
-      srcset=""
-    /></SwiperSlide>
-    <SwiperSlide><img
-      src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"
-      alt=""
-      height="180px"
-      width="100px"
-      srcset=""
-    /></SwiperSlide>
-    <SwiperSlide><img
-      src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"
-      alt=""
-      height="180px"
-      width="100px"
-      srcset=""
-    /></SwiperSlide>
-    <SwiperSlide><img
-      src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"
-      alt=""
-      height="180px"
-      width="100px"
-      srcset=""
-    /></SwiperSlide>
-    <SwiperSlide><img
-      src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"
-      alt=""
-      height="180px"
-      width="100px"
-      srcset=""
-    /></SwiperSlide>
-
-  </Swiper>
+    <SwiperSlide
+      ><img
+        src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"
+        alt=""
+        height="180px"
+        width="100px"
+        srcset=""
+      /></SwiperSlide
+    >
+    <SwiperSlide
+      ><img
+        src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"
+        alt=""
+        height="180px"
+        width="100px"
+        srcset=""
+      /></SwiperSlide
+    >
+    <SwiperSlide
+      ><img
+        src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"
+        alt=""
+        height="180px"
+        width="100px"
+        srcset=""
+      /></SwiperSlide
+    >
+    <SwiperSlide
+      ><img
+        src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"
+        alt=""
+        height="180px"
+        width="100px"
+        srcset=""
+      /></SwiperSlide
+    >
+    <SwiperSlide
+      ><img
+        src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"
+        alt=""
+        height="180px"
+        width="100px"
+        srcset=""
+      /></SwiperSlide
+    >
+  </Swiper> -->
 
   <Block />
 
-  <BlockTitle>Lastest  movies</BlockTitle>
+  <BlockTitle>Lastest movies</BlockTitle>
   <div>
     <Row noGap>
-      <Col
-        resizableAbsolute={true}
-        width="33"
-        xsmall="33"
-        small="33"
-        medium="20"
-        large="20"
-        xlarge="20"
-      >
-      <Link onClick={() => f7router.navigate('/movie/1')}>
-        <Card style="height: 180px; width: 100px; margin-right">
-          <CardContent padding={false}>
-            <div
-              class="bg-color-red"
-              style=" background-image: url(https://www.themoviedb.org/t/p/w300_and_h450_bestv2/xUuHj3CgmZQ9P2cMaqQs4J0d4Zc.jpg); no-repeat center top; background-size: 100px 180px; height: 160px;"
-            >
-            </div>
-            <div class="head-font">MOVIE TITLE THEasdsadsadasd UN FOF</div>
+      {#if movies.length}
+        {#each movies as movie}
+          <Col
+            resizableAbsolute={true}
+            width="33"
+            xsmall="33"
+            small="33"
+            medium="20"
+            large="20"
+            xlarge="20"
+          >
+            <Link onClick={() => f7router.navigate(`movie/${movie.pk}`)}>
+              <Card style="height: 180px; width: 100px; margin-right">
+                <CardContent padding={false}>
+                  <div
+                    class="bg-color-red"
+                    style="background-image: url('https://image.tmdb.org/t/p/w200{movie.poster_path}'); no-repeat center top; background-size: 100%; 180%; height: 180px;"
+                  />
+                  {#if movie.release_date}
+                    <div class="top-right-image">
+                      {new Date(movie.release_date).getFullYear()}
+                    </div>
+                  {/if}
 
-          </CardContent>
-        </Card>
-        </Link>
-        </Col
-      >
+                    {#if !movie.poster_path}
+                    <div class="no-title">No Poster :(</div>
 
+                    {/if}
+
+                  <div class="movie-title">{movie.title}</div>
+                </CardContent>
+              </Card>
+            </Link>
+          </Col>
+        {/each}
+      {:else}
+        <p>Something is strange...</p>
+      {/if}
     </Row>
   </div>
   <!-- 
@@ -178,15 +262,47 @@
   .head-font {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
       Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-      height: 20px;
-      width: 100px;
+    height: 20px;
+    width: 100px;
     padding-top: 4px;
     font-size: xx-small;
     color: rgb(255, 255, 255);
     text-shadow: #000000;
     justify-content: center;
     white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .movie-title {
+    background-color: #000000a9;
+    width: 100%;
+    position: absolute;
+    color: rgb(255, 255, 255);
+    position: absolute;
+    bottom: 0%;
+    font-size: 10px;
+  }
+
+  .top-right-image {
+    background-color: #000000a9;
+    border-radius: 0 0 0 5px;
+    font-style: oblique;
+    color: rgb(255, 255, 255);
+    position: absolute;
+    top: 0px;
+    padding: 0.2rem;
+    right: 0px;
+    font-size: 10px;
+  }
+
+  .no-title {
+    border-radius: 0 0 0 5px;
+    font-style: oblique;
+    color: rgb(255, 255, 255);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
 </style>
--- src/pages/movie-page.svelte
+++ src/pages/movie-page.svelte
@@ -1,28 +1,58 @@
 <script>
-    import { Page,  Card, Navbar,CardHeader, CardContent, CardFooter, Link, List, ListItem } from 'framework7-svelte';
-    export let f7router;
-    export let f7route;
-    const movieId = f7route.params.id;
+  import {
+    Page,
+    Card,
+    Navbar,
+    CardHeader,
+    CardContent,
+    CardFooter,
+    Link,
+    List,
+    ListItem,
+  } from "framework7-svelte";
+  import { onMount } from "svelte";
+  import axios from "axios";
 
-  </script>
+  export let f7router;
+  export let f7route;
+  const movieId = f7route.params.id;
+  let movie = [];
+
+  onMount(async () => {
+    const res = await axios.get(
+      "http://127.0.0.1:8000/movies/movies/" + f7route.params.id
+    );
+    movie = res.data;
+    console.log(res);
+  });
+</script>
+
 <Page>
   <Navbar title="Movie" />
 
-<Card card-opened>
+  <Card card-opened>
     <CardContent padding={false}>
-      <div class="bg-color-yellow" style="height: 300px">
+      <div
+        class="bg-color-yellow"
+        style="background-image: url('https://image.tmdb.org/t/p/w500{movie.poster_path}'); no-repeat center top; background-size: 100%; 100%; height: 300px;"
+      >
         <CardHeader textColor="black" class="display-block">
-          Movie Name {movieId}
-          <br/>
+          {movie.title}
+          <br />
           <small style="opacity: 0.7">A little bit of comment</small>
         </CardHeader>
-        <Link onClick={() => f7router.back()} cardClose color="black" class="card-opened-fade-in" style="position: absolute; right: 15px; top: 15px" iconF7="xmark_circle_fill" />
+        <Link
+          onClick={() => f7router.back()}
+          cardClose
+          color="black"
+          class="card-opened-fade-in"
+          style="position: absolute; right: 15px; top: 15px"
+          iconF7="xmark_circle_fill"
+        />
       </div>
+
       <div class="card-content-padding">
-        <p>Framework7 - is a free and open source HTML mobile framework to develop hybrid mobile apps or web apps with iOS or Android (Material) native look and feel. It is also an indispensable prototyping apps tool to show working app prototype as soon as possible in case you need to. Framework7 is created by Vladimir Kharlampidi (iDangero.us).</p>
-        <p>The main approach of the Framework7 is to give you an opportunity to create iOS and Android (Material) apps with HTML, CSS and JavaScript easily and clear. Framework7 is full of freedom. It doesn't limit your imagination or offer ways of any solutions somehow. Framework7 gives you freedom!</p>
-        <p>Framework7 is not compatible with all platforms. It is focused only on iOS and Android (Material) to bring the best experience and simplicity.</p>
-        <p>Framework7 is definitely for you if you decide to build iOS and Android hybrid app (Cordova or Capacitor) or web app that looks like and feels as great native iOS or Android (Material) apps.</p>
+        <p>{movie.overview}</p>
       </div>
     </CardContent>
   </Card>
